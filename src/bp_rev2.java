93ee06868bf61b1baa907dddff40ef6db179c296
the1337tum <tim.blackburn09@gmail.com>
2017-10-30 12:45:42 +0800

    foundation to build sigmoid + bias

--- src/bp_rev2.java
+++ src/bp_rev2.java
@@ -8,6 +8,7 @@
 	// https://www.youtube.com/watch?v=IHZwWFHWa-w
 	// https://stats.stackexchange.com/questions/185071/can-neural-network-e-g-convolutional-neural-network-have-negative-weights
 	// http://neuralnetworksanddeeplearning.com/chap2.html
+	// https://www.youtube.com/watch?v=L_PByyJ9g-I
 
 public class BackProp {
 	// Edges are initialised to values in the range +/- 0.3
@@ -35,112 +36,108 @@ public class BackProp {
 	double[] t;	// target value                 (immutable)
 
 
-	private double activate(double value) {
-		return 1 / (1 + Math.exp(-value));
-		// ReLU Activation
-		// return Math.max(0.001 * value, value);
-	}
-
-	private double derivative(double value) {
-		return value * (1 - value);
-		// ReLU Derivative
-		// if (value > 0) 
-		// 	return 1;
-		// else
-		//	return 0.001;
-	}
-	
 	// n = node, e = edge
 	BackProp(int i_len, int h_len, int o_len) {
 		// node activations
-		i = new double[i_len];
-		for (int n = 0; n < i_len; n++)
-			i[n] = 1.0;
 		h = new double[h_len];
 		for (int n = 0; n < h_len; n++)
-			h[n] = 1.0;
+			h[n] = -1.0 + Math.random() * 2;
 		o = new double[o_len];
 		for (int n = 0; n < o_len; n++)
-			o[n] = 1.0;
+			o[n] = -1.0 + Math.random() * 2;
 
 		// bias activations
 		h_bias = new double[h_len];
 		for (int n = 0; n < h_len; n++)
-			h_bias[n] = -0.5 + Math.random();
+			h_bias[n] = 0;//-10.5 + Math.random() * 20;
 		o_bias = new double[o_len];
 		for (int n = 0; n < o_len; n++)
-			o_bias[n] = -0.5 + Math.random();
+			o_bias[n] = 0;//-10.5 + Math.random() * 20;
 
 		// edge activations
 		ih = new double[i_len][h_len];
 		for (int n = 0; n < i_len; n++)
 			for (int e = 0; e < h_len; e++)
-				ih[n][e] = -0.5 + Math.random();
+				ih[n][e] = -0.5 + Math.random() * 2;
 		ho = new double[h_len][o_len];
 		for (int n = 0; n < h_len; n++)
 			for (int e = 0; e < o_len; e++)
-				ho[n][e] = -0.5 + Math.random();
+				ho[n][e] = -0.5 + Math.random() * 2;
 		
 		delta[0] = new double[o_len];
 		delta[1] = new double[h_len];
 	}
 
+	private double activate(double value) {
+		// Tanh Activation
+		return Math.tanh(value);
+		// Sigmoid Activation
+		//return 1.0 / (1.0 + Math.exp(-value));
+		// ReLU Activation
+		// return Math.max(0.001 * value, value);
+	}
+
+	private double derivative(double value) {
+		// Tanh Derivative
+		return 1 - Math.pow(value, 2);
+		// Sigmoid Derivative
+		//return activate(value) * (1.0 - activate(value));
+		// ReLU Derivative
+		// if (value > 0) 
+		//	return 1;
+		// else
+		//	return 0.001;
+	}
+	
 	// delta[0] = output delta
 	// delta[1] = hidden delta
 	double back_prop() {
-		double error = 0.0;
-		
 		// output error
 		for (int n = 0; n < o.length; n++) {
-			delta[0][n] = (t[n] - o[n]) * derivative(o[n]);
-			error += delta[0][n];
-			System.out.println("o[n]: " + o[n]);
-			// System.out.println("delta: " + delta[0][n]);
+			delta[0][n] = -(t[n] - o[n]) * derivative(o[n]);
 		}
-		
+
 		// hidden-output error
 		for (int n = 0; n < h.length; n++) {
 			for (int e = 0; e < o.length; e++) {
-				ho[n][e] -= delta[0][e] * o[e];
-				o_bias[e] -= delta[0][e] / error;
+				ho[n][e] -= RATE * delta[0][e] * h[n];
 			}
 		}
 		
 		// hidden error
-		double sum = 0;
 		for (int n = 0; n < h.length; n++) {
+			double sum = 0;
 			for (int e = 0; e < o.length; e++) {
-				sum += h[e] * ho[n][e];
+				sum += delta[0][e] * ho[n][e];
 			}
-			delta[1][n] = derivative(sum);
-			System.out.println("delta[1][n]: " + delta[1][n]);
+			delta[1][n] = sum * derivative(h[n]);
 		}
-		
+
 		// input-hidden error
 		for (int n = 0; n < i.length; n++) {
 			for (int e = 0; e < h.length; e++) {
-				ih[n][e] -= delta[1][e];
+				ih[n][e] -= RATE * delta[1][e] * i[n];
 			}
 		}
-		
-		return error;
+		return 0.0;
 	}
 
     void push_forward() {
     	// hidden activations
-    	int sum = 0;
+    	double sum = 0;
     	for (int e = 0; e < h.length; e++) {
     		for (int n = 0; n < i.length; n++) {
-    			sum += i[n] * ih[n][e] - h_bias[e];   // ih[h_len][i_len]
+    			sum += i[n] * ih[n][e];
 		}
-		h[e] = activate(sum);
+		h[e] = activate(sum + h_bias[e]);
     	}
     	// output activations
     	sum = 0;
     	for (int e = 0; e < o.length; e++) {
-    		for (int n = 0; n < h.length; n++)
-    			sum += h[n] * ho[n][e] - o_bias[e]; 
-    		o[e] = activate(sum);
+    		for (int n = 0; n < h.length; n++) {
+			sum += h[n] * ho[n][e];
+		}
+    		o[e] = activate(sum + o_bias[e]);
     	}
     }
     
